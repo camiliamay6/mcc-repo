41083c5ef7200034bcbb7f9c67963f759d79517d
Meila Kamilia <meilakamilia06@gmail.com>
2022-05-25 09:56:34 +0700

    FIX BUG  - Add comment and simplify code

--- repo-practice/Program.cs
+++ repo-practice/Program.cs
@@ -9,22 +9,22 @@ namespace repo_practice
         {
             Repositories.EmployeeRepository emp_obj = new Repositories.EmployeeRepository();
             Repositories.ItemRepository item_r = new Repositories.ItemRepository();
-            
+
             emp_obj.MakeNewConnection();
             item_r.MakeNewConnection();
 
-         
+
 
             Models.Employee model_e = new Models.Employee();
             Models.Item model_i = new Models.Item();
 
             //Insert Data
             /*Random rnd = new Random();
-            model_e.id = rnd.Next();
+            model_e.id = 251660114;
             model_e.employee_name = "'Sinta'";
             model_e.employee_address = "'Jakarta'";
 
-            model_i.Id = rnd.Next();
+            model_i.Id = 607222773;
             model_i.item_name = "'Bottle'";
             model_i.price = 15000;
             model_i.quantity = 20;
@@ -63,7 +63,7 @@ namespace repo_practice
 
             //Retrieve All data
 
-            Dictionary<int, List<string>> result_i = item_r.GetAll(model_i);
+            /*Dictionary<int, List<string>> result_i = item_r.GetAll(model_i);
             foreach (var item in result_i)
             {
                 foreach (var i in item.Value)
@@ -82,50 +82,13 @@ namespace repo_practice
                     Console.WriteLine(i);
                 }
                 Console.WriteLine("--------------");
-            }
-
-/*    Random rnd = new Random();
-    model_e.id = rnd.Next();
-    model_e.employee_name = "'Sinta'";
-    model_e.employee_address = "'Jakarta'";
-
-    emp_obj.Create(model_e);*/
-/*
-            Dictionary<int, List<string>> result = item_r.GetAll(model_i);
-            foreach (var item in result)
-            {
-                foreach (var i in item.Value)
-                {
-                    Console.WriteLine(i);
-                }
-                Console.WriteLine("--------------");
             }*/
-/* Repositories.ItemRepository item_r = new Repositories.ItemRepository();
- item_r.MakeNewConnection();
- Models.Item model_i = new Models.Item();
- List<string> result = item_r.GetById(model_i, 1);
- foreach (var item in result)
- {
-     Console.WriteLine(item);
- }
-*/
-/*            Random rnd = new Random();
-            Models.Item model_i = new Models.Item();
-            model_i.Id = rnd.Next();
-            model_i.item_name = "'Bottle'";
-            model_i.price = 50000;
-            model_i.quantity = 30;
 
-            item_r.Create(model_i);*/
+            //Delete one data
 
-/* Models.Item model_i = new Models.Item();
- model_i.Id = 1;
- model_i.item_name = "'Book'";
- model_i.price = 5000;
- model_i.quantity = 20;
-*/
-/*  Models.Item model_i = new Models.Item();
-  item_r.Delete(1463969350, model_i);*/
+            /* item_r.Delete(607222773, model_i);
+             emp_obj.Delete(251660114, model_e);*/
+
             emp_obj.CloseConnection();
             item_r.CloseConnection();
         }
